6caf4501f620d4ec589d9ba66974a15d5382f251
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2021-04-02 04:44:52 -0400

    Planned stuff and opengl learning

---
+++ README.md
@@ -0,0 +1,16 @@
+# snow-64
+Snow-64 is a fantasy console.
+
+# Features (Planned)
+
+- 16-bit color (R - 4 bits, G - 4 bits, B - 4 bits, A - 4 bits)
+- 512 x 512 screen resolution.
+- 16 x 16 sprites with 4 layers. (Text, Entities, Entities or Tiles, Tiles)
+- 512 x 512 tile map.
+- 4 tile maps with 32 tiles each.
+- Scripting with rhai, wren, and typescript.
+- Some kind of sound.
+- Some kind of cart size. (Carts will have their own format)
+- Direct pixel draw mode. (Enabled per layer)
+- Its own shell, with a basic file system.
+- Its own pixel art program, code editor, and build system (flake).
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,111 @@
+#[macro_use]
+extern crate glium;
+extern crate image;
+
+#[derive(Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+    tex_coords: [f32; 2]
+}
+implement_vertex!(Vertex, position, tex_coords);
+
 fn main() {
     println!("Hello, world!");
+
+    use glium::{glutin, Surface};
+
+    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    //
+    use std::io::Cursor;
+    let image = image::load(Cursor::new(&include_bytes!("button.png")[..]), image::ImageFormat::Png).unwrap().to_rgba8();
+    let image_dims = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dims);
+
+    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
+    //
+
+    //
+    let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 1.0] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25], tex_coords: [1.0, 0.0] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+        in vec2 tex_coords;
+        out vec2 v_tex_coords;
+
+        uniform mat4 matrix;
+
+        void main() {
+            v_tex_coords = tex_coords;
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
+        }
+    "#;
+    let frag_shader_src = r#"
+        #version 140
+
+        in vec2 v_tex_coords;
+        out vec4 color;
+
+        uniform sampler2D tex;
+
+        void main() {
+            color = texture(tex, v_tex_coords);
+        }
+    "#;
+    let program = glium::Program::from_source(&display, vertex_shader_src, frag_shader_src, None).unwrap();
+    //
+
+    let mut t: f32 = -0.5;
+
+    event_loop.run(move |ev, _, control_flow| {
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return
+            },
+            glutin::event::Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => return
+        }
+
+        let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        t += 0.002;
+        if t > 0.5 {
+            t = -0.5;
+        }
+
+        let uniforms = uniform! {
+            matrix: [
+                [ t.cos(),  t.sin(), 0.0, 0.0],
+                [-t.sin(),  t.cos(), 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [ t , 0.0, 0.0, 1.0_f32],
+            ],
+            tex: &texture
+        };
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+        target.finish().unwrap();
+    });
 }
