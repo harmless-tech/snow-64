ae65f9aca00f8df1d807d21a7255bb97279a4ef1
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2021-04-02 14:14:28 -0400

    Meh

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
+mod teapot;
+
 #[macro_use]
 extern crate glium;
 extern crate image;
+
+use glium::glutin::dpi::{Size, PhysicalSize};
+use std::borrow::Cow;
+use glium::texture::Texture2dDataSink;
 
 #[derive(Copy, Clone)]
 struct Vertex {
@@ -15,20 +21,105 @@ fn main() {
     use glium::{glutin, Surface};
 
     let mut event_loop = glutin::event_loop::EventLoop::new();
-    let wb = glutin::window::WindowBuilder::new();
-    let cb = glutin::ContextBuilder::new();
+    let wb = glutin::window::WindowBuilder::new().with_title("snow-64").with_resizable(false).with_inner_size(Size::Physical(PhysicalSize { width: 512, height: 512 }));
+    let cb = glutin::ContextBuilder::new().with_depth_buffer(24);
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
     //
-    use std::io::Cursor;
-    let image = image::load(Cursor::new(&include_bytes!("button.png")[..]), image::ImageFormat::Png).unwrap().to_rgba8();
+    /*use std::io::Cursor;
+    let image = image::load(Cursor::new(&include_bytes!("../button.png")[..]), image::ImageFormat::Png).unwrap().to_rgba8();
     let image_dims = image.dimensions();
     let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dims);
 
-    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
+    let texture = glium::texture::Texture2d::new(&display, image).unwrap();*/
     //
 
     //
+    /*let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 1.0] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25], tex_coords: [1.0, 0.0] };
+    let shape = vec![vertex1, vertex2, vertex3];*/
+
+    /*let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);*/
+
+    /*let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+        in vec2 tex_coords;
+        out vec2 v_tex_coords;
+
+        uniform mat4 matrix;
+
+        void main() {
+            v_tex_coords = tex_coords;
+            gl_Position = matrix * vec4(position, 0.0, 1.0);
+        }
+    "#;
+    let frag_shader_src = r#"
+        #version 140
+
+        in vec2 v_tex_coords;
+        out vec4 color;
+
+        uniform sampler2D tex;
+
+        void main() {
+            color = texture(tex, v_tex_coords);
+        }
+    "#;
+    let program = glium::Program::from_source(&display, vertex_shader_src, frag_shader_src, None).unwrap();*/
+    //
+
+    //
+    /*let positions = glium::VertexBuffer::new(&display, &teapot::VERTICES).unwrap();
+    let normals = glium::VertexBuffer::new(&display, &teapot::NORMALS).unwrap();
+    let indices = glium::IndexBuffer::new(&display, glium::index::PrimitiveType::TrianglesList, &teapot::INDICES).unwrap();
+
+    let vertex_shader_src = r#"
+        #version 150
+
+        in vec3 position;
+        in vec3 normal;
+
+        out vec3 v_normal;
+
+        uniform mat4 perp;
+        uniform mat4 matrix;
+
+        void main() {
+            v_normal = transpose(inverse(mat3(matrix))) * normal;
+            gl_Position = perp * matrix * vec4(position, 1.0);
+        }
+    "#;
+    let frag_shader_src = r#"
+        #version 140
+
+        in vec3 v_normal;
+        out vec4 color;
+        uniform vec3 u_light;
+
+        void main() {
+            float brightness = dot(normalize(v_normal), normalize(u_light));
+            vec3 dark_color = vec3(0.6, 0.0, 0.0);
+            vec3 reg_color = vec3(1.0, 0.0, 0.0);
+            color = vec4(mix(dark_color, reg_color, brightness), 1.0);
+        }
+    "#;
+    let program = glium::Program::from_source(&display, vertex_shader_src, frag_shader_src, None).unwrap();*/
+    //
+
+    //
+    let image = glium::texture::RawImage2d::from_raw(Cow::from(vec![(1_u8, 1_u8, 1_u8, 1_u8)]), 1, 1);
+    let texture = glium::texture::Texture2d::new(&display, image).unwrap();
+
+    /*let vertex1 = Vertex { position: [-1.0,  1.0] };
+    let vertex2 = Vertex { position: [-1.0, -1.0] };
+    let vertex3 = Vertex { position: [ 1.0, -1.0] };
+    let vertex4 = Vertex { position: [ 1.0,  1.0] };
+    let shape = vec![vertex1, vertex2, vertex3, vertex4];*/
+
     let vertex1 = Vertex { position: [-0.5, -0.5], tex_coords: [0.0, 0.0] };
     let vertex2 = Vertex { position: [ 0.0,  0.5], tex_coords: [0.0, 1.0] };
     let vertex3 = Vertex { position: [ 0.5, -0.25], tex_coords: [1.0, 0.0] };
@@ -38,7 +129,7 @@ fn main() {
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
 
     let vertex_shader_src = r#"
-        #version 140
+        #version 150
 
         in vec2 position;
         in vec2 tex_coords;
@@ -66,7 +157,31 @@ fn main() {
     let program = glium::Program::from_source(&display, vertex_shader_src, frag_shader_src, None).unwrap();
     //
 
-    let mut t: f32 = -0.5;
+    /*let params = glium::DrawParameters {
+        depth: glium::Depth {
+            test: glium::draw_parameters::DepthTest::IfLess,
+            write: true,
+            .. Default::default()
+        },
+        .. Default::default()
+    };*/
+    let params = glium::DrawParameters {
+        depth: glium::Depth {
+            test: glium::DepthTest::IfLess,
+            write: true,
+            .. Default::default()
+        },
+        backface_culling: glium::draw_parameters::BackfaceCullingMode::CullClockwise,
+        .. Default::default()
+    };
+
+    /*let matrix = [
+        [0.01, 0.0, 0.0, 0.0],
+        [0.0, 0.01, 0.0, 0.0],
+        [0.0, 0.0, 0.01, 0.0],
+        [0.0, 0.0, 2.0, 1.0f32]
+    ];
+    let light = [-1.0, 0.4, 0.9f32];*/
 
     event_loop.run(move |ev, _, control_flow| {
         match ev {
@@ -88,24 +203,41 @@ fn main() {
         let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_667);
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
-        t += 0.002;
-        if t > 0.5 {
-            t = -0.5;
-        }
+        let mut target = display.draw();
+
+        /*let perp = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
+
+            let fov: f32 = 3.141592 / 3.0;
+            let zfar = 1024.0;
+            let znear = 0.1;
+
+            let f = 1.0 / (fov / 2.0).tan();
+
+            [
+                [f * aspect_ratio, 0.0, 0.0, 0.0],
+                [0.0, f, 0.0, 0.0],
+                [0.0, 0.0, (zfar + znear) / (zfar - znear), 1.0],
+                [0.0, 0.0, -(2.0 * zfar * znear) / (zfar - znear), 0.0],
+            ]
+        };*/
 
         let uniforms = uniform! {
             matrix: [
-                [ t.cos(),  t.sin(), 0.0, 0.0],
-                [-t.sin(),  t.cos(), 0.0, 0.0],
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
                 [0.0, 0.0, 1.0, 0.0],
-                [ t , 0.0, 0.0, 1.0_f32],
+                [0.0, 0.0, 0.0, 1.0f32],
             ],
             tex: &texture
         };
 
-        let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        // target.clear_color(0.0, 1.0, 1.0, 1.0);
+        target.clear_color_and_depth((0.0, 1.0, 1.0, 1.0), 1.0);
         target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+        // target.draw(&vertex_buffer, &indices, &program, &uniforms, &Default::default()).unwrap();
+        // target.draw((&positions, &normals), &indices, &program, &uniform! { matrix: matrix, perp: perp, u_light: light }, &params).unwrap();
         target.finish().unwrap();
     });
 }
